f5b4f83e81acc714a530477888f693c3976039d8
Guanheng Zhang <zhangguanheng@devfair0129.h2.fair>
2019-11-08 09:02:27 -0800

    Docs.

--- torchtext/datasets/translation.py
+++ torchtext/datasets/translation.py
@@ -9,7 +9,8 @@ from torchtext.data.functional import read_text_iterator, create_data_from_itera
 URLS = {
     'Multi30k': ['http://www.quest.dcs.shef.ac.uk/wmt16_files_mmt/training.tar.gz',
                  'http://www.quest.dcs.shef.ac.uk/wmt16_files_mmt/validation.tar.gz',
-                 'http://www.quest.dcs.shef.ac.uk/wmt17_files_mmt/mmt_task1_test2016.tar.gz'],
+                 'http://www.quest.dcs.shef.ac.uk/wmt17_files_mmt/'
+                 'mmt_task1_test2016.tar.gz'],
     'WMT14': 'https://drive.google.com/uc?export=download&id=0B_bZck-ksdkpM25jRUN2X2UxMm8'
 }
 
@@ -70,12 +71,14 @@ class TranslationDataset(torch.utils.data.Dataset):
     """
 
     def __init__(self, data, vocab):
-        """Initiate language modeling dataset.
+        """Initiate translation dataset.
 
         Arguments:
             data: a tuple of source and target tensors, which include token ids
                 numericalizing the string tokens.
-            vocab: Vocabulary object used for dataset.
+                [(src_tensor0, tgt_tensor0), (src_tensor1, tgt_tensor1)]
+            vocab: source and target Vocabulary object used for dataset.
+                (src_vocab, tgt_vocab)
 
         Examples:
             >>> from torchtext.vocab import build_vocab_from_iterator
@@ -83,8 +86,10 @@ class TranslationDataset(torch.utils.data.Dataset):
                                          token_id_s3, token_id_s1]).long()
             >>> tgt_data = torch.Tensor([token_id_t1, token_id_t2,
                                          token_id_t3, token_id_t1]).long()
-            >>> vocab = build_vocab_from_iterator([['language', 'modeling']])
-            >>> dataset = LanguageModelingDataset([(src_data, tgt_data)], vocab)
+            >>> src_vocab = build_vocab_from_iterator([['Übersetzungsdatensatz']])
+            >>> tgt_vocab = build_vocab_from_iterator([['translation', 'dataset']])
+            >>> dataset = TranslationDataset([(src_data, tgt_data)],
+                                              (src_vocab, tgt_vocab))
         """
 
         super(TranslationDataset, self).__init__()
@@ -105,33 +110,45 @@ class TranslationDataset(torch.utils.data.Dataset):
         return self._vocab
 
 
-def Multi30k(*args, **kwargs):
-    """ Defines IMDB datasets.
-        The labels includes:
-            - 0 : Negative
-            - 1 : Positive
-    Create sentiment analysis dataset: IMDB
-    Separately returns the training and test dataset
+def Multi30k(tokenizer=(get_tokenizer("spacy", language='de'),
+                        get_tokenizer("spacy", language='en')),
+             root='.data', vocab=(None, None),
+             removed_tokens=['<unk>']):
+
+    """ Define translation datasets: Multi30k
+        Separately returns train/valid/test datasets
+
     Arguments:
-        tokenizer: the tokenizer used to preprocess raw text data.
-            The default one is basic_english tokenizer in fastText. spacy tokenizer
-            is supported as well (see example below). A custom tokenizer is callable
-            function with input of a string and output of a token list.
+        train_filenames: the source and target filenames for training.
+            Default: ('train.de', 'train.en')
+        valid_filenames: the source and target filenames for valid.
+            Default: ('val.de', 'val.en')
+        test_filenames: the source and target filenames for test.
+            Default: ('test2016.de', 'test2016.en')
+        tokenizer: the tokenizer used to preprocess source and target raw text data.
+            Default: (torchtext.data.utils.get_tokenizer("spacy", language='de'),
+                      torchtext.data.utils.get_tokenizer("spacy", language='en'))
         root: Directory where the datasets are saved. Default: ".data"
-        vocab: Vocabulary used for dataset. If None, it will generate a new
-            vocabulary based on the train data set.
+        vocab: Source and target Vocabulary objects used for dataset. If None, it
+            will generate a new vocabulary based on the train data set.
         removed_tokens: removed tokens from output dataset (Default: '<unk>')
+
     Examples:
-        >>> from torchtext.datasets import IMDB
+        >>> from torchtext.datasets import Multi30k
         >>> from torchtext.data.utils import get_tokenizer
-        >>> tokenizer = get_tokenizer("spacy")
-        >>> train_dataset, test_dataset = IMDB(tokenizer=tokenizer)
-        >>> vocab = train_dataset.get_vocab()
+        >>> tokenizer = (get_tokenizer("spacy", language='de'),
+                         get_tokenizer("basic_english"))
+        >>> train_dataset, valid_dataset, test_dataset = Multi30k(tokenizer=tokenizer)
+        >>> src_vocab, tgt_vocab = train_dataset.get_vocab()
+        >>> src_data, tgt_data = train_dataset[10]
     """
 
-    train_dataset = _setup_datasets(*((URLS['Multi30k'][0],) + args), **kwargs)
-    valid_dataset = _setup_datasets(*((URLS['Multi30k'][1],) + args), **kwargs)
-    test_dataset = _setup_datasets(*((URLS['Multi30k'][2],) + args), **kwargs)
+    train_dataset = _setup_datasets(URLS['Multi30k'][0], ('train.de', 'train.en'),
+                                    tokenizer, root, vocab, removed_tokens)
+    valid_dataset = _setup_datasets(URLS['Multi30k'][1], ('val.de', 'val.en'),
+                                    tokenizer, root, vocab, removed_tokens)
+    test_dataset = _setup_datasets(URLS['Multi30k'][2], ('test2016.de', 'test2016.en'),
+                                   tokenizer, root, vocab, removed_tokens)
     return (train_dataset, valid_dataset, test_dataset)
 
 
@@ -145,34 +162,86 @@ def WMT14(train_filenames=('train.tok.clean.bpe.32000.de',
                      get_tokenizer("spacy", language='en')),
           root='.data', vocab=(None, None),
           removed_tokens=['<unk>']):
-    """ Defines IMDB datasets.
-        The labels includes:
-            - 0 : Negative
-            - 1 : Positive
-    Create sentiment analysis dataset: IMDB
-    Separately returns the training and test dataset
+
+    """ Define translation datasets: WMT14
+        Separately returns train/valid/test datasets
+        The available datasets include:
+            newstest2016.en
+            newstest2016.de
+            newstest2015.en
+            newstest2015.de
+            newstest2014.en
+            newstest2014.de
+            newstest2013.en
+            newstest2013.de
+            newstest2012.en
+            newstest2012.de
+            newstest2011.tok.de
+            newstest2011.en
+            newstest2011.de
+            newstest2010.tok.de
+            newstest2010.en
+            newstest2010.de
+            newstest2009.tok.de
+            newstest2009.en
+            newstest2009.de
+            newstest2016.tok.de
+            newstest2015.tok.de
+            newstest2014.tok.de
+            newstest2013.tok.de
+            newstest2012.tok.de
+            newstest2010.tok.en
+            newstest2009.tok.en
+            newstest2015.tok.en
+            newstest2014.tok.en
+            newstest2013.tok.en
+            newstest2012.tok.en
+            newstest2011.tok.en
+            newstest2016.tok.en
+            newstest2009.tok.bpe.32000.en
+            newstest2011.tok.bpe.32000.en
+            newstest2010.tok.bpe.32000.en
+            newstest2013.tok.bpe.32000.en
+            newstest2012.tok.bpe.32000.en
+            newstest2015.tok.bpe.32000.en
+            newstest2014.tok.bpe.32000.en
+            newstest2016.tok.bpe.32000.en
+            train.tok.clean.bpe.32000.en
+            newstest2009.tok.bpe.32000.de
+            newstest2010.tok.bpe.32000.de
+            newstest2011.tok.bpe.32000.de
+            newstest2013.tok.bpe.32000.de
+            newstest2012.tok.bpe.32000.de
+            newstest2014.tok.bpe.32000.de
+            newstest2016.tok.bpe.32000.de
+            newstest2015.tok.bpe.32000.de
+            train.tok.clean.bpe.32000.de
 
     Arguments:
-        train_filenames:
-        valid_filenames:
-        test_filenames:
-        tokenizer: the tokenizer used to preprocess raw text data.
-            The default one is basic_english tokenizer in fastText. spacy tokenizer
-            is supported as well (see example below). A custom tokenizer is callable
-            function with input of a string and output of a token list.
+        train_filenames: the source and target filenames for training.
+            Default: ('train.tok.clean.bpe.32000.de', 'train.tok.clean.bpe.32000.en')
+        valid_filenames: the source and target filenames for valid.
+            Default: ('newstest2013.tok.bpe.32000.de', 'newstest2013.tok.bpe.32000.en')
+        test_filenames: the source and target filenames for test.
+            Default: ('newstest2014.tok.bpe.32000.de', 'newstest2014.tok.bpe.32000.en')
+        tokenizer: the tokenizer used to preprocess source and target raw text data.
+            Default: (torchtext.data.utils.get_tokenizer("spacy", language='de'),
+                      torchtext.data.utils.get_tokenizer("spacy", language='en'))
         root: Directory where the datasets are saved. Default: ".data"
-        vocab: Vocabulary used for dataset. If None, it will generate a new
-            vocabulary based on the train data set.
+        vocab: Source and target Vocabulary objects used for dataset. If None, it
+            will generate a new vocabulary based on the train data set.
         removed_tokens: removed tokens from output dataset (Default: '<unk>')
 
     Examples:
-        >>> from torchtext.datasets import IMDB
+        >>> from torchtext.datasets import WMT14
         >>> from torchtext.data.utils import get_tokenizer
-        >>> tokenizer = get_tokenizer("spacy")
-        >>> train_dataset, test_dataset = IMDB(tokenizer=tokenizer)
-        >>> vocab = train_dataset.get_vocab()
+        >>> src_tokenizer = get_tokenizer("spacy", language='de')
+        >>> tgt_tokenizer = get_tokenizer("basic_english")
+        >>> train_dataset, valid_dataset, test_dataset = WMT14(tokenizer=(src_tokenizer,
+                                                                          tgt_tokenizer))
+        >>> src_vocab, tgt_vocab = train_dataset.get_vocab()
+        >>> src_data, tgt_data = train_dataset[10]
     """
-
     train_dataset = _setup_datasets(URLS['WMT14'], train_filenames,
                                     tokenizer, root, vocab, removed_tokens)
     valid_dataset = _setup_datasets(URLS['WMT14'], valid_filenames,
