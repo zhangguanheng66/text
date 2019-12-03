4ab27919903606b25af21e7a0d453b19d7a8dd60
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2019-12-03 07:28:21 -0800

    Move language modeling dataset to prototype.

--- torchtext/prototype/datasets/__init__.py
+++ torchtext/prototype/datasets/__init__.py
@@ -1,9 +1,15 @@
+from .language_modeling import LanguageModelingDataset, \
+    WikiText2, WikiText103, PennTreebank
 from .text_classification import TextClassificationDataset, \
     AG_NEWS, SogouNews, DBpedia, YelpReviewPolarity, \
     YelpReviewFull, YahooAnswers, \
     AmazonReviewPolarity, AmazonReviewFull, IMDB
 
-__all__ = ['TextClassificationDataset',
+__all__ = ['LanguageModelingDataset',
+           'WikiText2',
+           'WikiText103',
+           'PennTreebank',
+           'TextClassificationDataset',
            'AG_NEWS',
            'SogouNews',
            'DBpedia',
