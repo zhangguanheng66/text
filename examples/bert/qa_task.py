e3b1831d75c3ab5143247f07104b638268bfae27
Guanheng Zhang <zhangguanheng@devfair0197.h2.fair>
2020-02-14 12:00:53 -0800

    update qa_task

--- examples/bert/qa_task.py
+++ examples/bert/qa_task.py
@@ -1,12 +1,12 @@
-# coding: utf-8
 import argparse
 import time
 import math
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
-
-import model
+import torchtext
+from data import SQuAD
+from model import QuestionAnswerTask
 
 
 def pad_squad_data(batch):
@@ -100,49 +100,10 @@ def train():
             start_time = time.time()
 
 
-# At any point you can hit Ctrl + C to break out of training early.
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='PyTorch Wikitext-2 Transformer Language Model')
+    parser = argparse.ArgumentParser(description='Question-Answer fine-tuning task')
     parser.add_argument('--data', type=str, default='./data/wikitext-2',
                         help='location of the data corpus')
-    #parser.add_argument('--emsize', type=int, default=200,
-    #                    help='size of word embeddings')
-    #parser.add_argument('--nhid', type=int, default=200,
-    #                    help='number of hidden units per layer')
-    #parser.add_argument('--nlayers', type=int, default=2,
-    #                    help='number of layers')
-    #parser.add_argument('--lr', type=float, default=20,
-    #                    help='initial learning rate')
-    #parser.add_argument('--clip', type=float, default=0.25,
-    #                    help='gradient clipping')
-    #parser.add_argument('--epochs', type=int, default=40,
-    #                    help='upper epoch limit')
-    #parser.add_argument('--batch_size', type=int, default=20, metavar='N',
-    #                    help='batch size')
-    #parser.add_argument('--dropout', type=float, default=0.2,
-    #                    help='dropout applied to layers (0 = no dropout)')
-    #parser.add_argument('--tied', action='store_true',
-    #                    help='tie the word embedding and softmax weights')
-    #parser.add_argument('--seed', type=int, default=1111,
-    #                    help='random seed')
-    #parser.add_argument('--cuda', action='store_true',
-    #                    help='use CUDA')
-    #parser.add_argument('--log-interval', type=int, default=200, metavar='N',
-    #                    help='report interval')
-    #parser.add_argument('--save', type=str, default='model.pt',
-    #                    help='path to save the final model')
-    #
-    #parser.add_argument('--nhead', type=int, default=2,
-    #                    help='the number of heads in the encoder/decoder of the transformer model')
-
-    # For test
-    parser.add_argument('--emsize', type=int, default=32,
-                        help='size of word embeddings')
-    parser.add_argument('--nhid', type=int, default=64,
-                        help='number of hidden units per layer')
-    parser.add_argument('--nlayers', type=int, default=4,
-                        help='number of layers')
     parser.add_argument('--lr', type=float, default=20,
                         help='initial learning rate')
     parser.add_argument('--clip', type=float, default=0.25,
@@ -151,10 +112,6 @@ if __name__ == "__main__":
                         help='upper epoch limit')
     parser.add_argument('--batch_size', type=int, default=8, metavar='N',
                         help='batch size')
-    parser.add_argument('--dropout', type=float, default=0.2,
-                        help='dropout applied to layers (0 = no dropout)')
-    parser.add_argument('--tied', action='store_true',
-                        help='tie the word embedding and softmax weights')
     parser.add_argument('--seed', type=int, default=1111,
                         help='random seed')
     parser.add_argument('--cuda', action='store_true',
@@ -167,39 +124,16 @@ if __name__ == "__main__":
                         help='path to save the vocab')
     parser.add_argument('--bert-model', type=str,
                         help='path to save the pretrained bert')
-
-    parser.add_argument('--nhead', type=int, default=8,
-                        help='the number of heads in the encoder/decoder of the transformer model')
-
-    parser.add_argument('--mask_frac', type=float, default=0.15,
-                        help='the fraction of masked tokens')
     args = parser.parse_args()
 
     # Set the random seed manually for reproducibility.
     torch.manual_seed(args.seed)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-###############################################################################
-# Load data
-###############################################################################
+    ###############################################################################
+    # Load data
+    ###############################################################################
 
-# Starting from sequential data, batchify arranges the dataset into columns.
-# For instance, with the alphabet as the sequence and batch size 4, we'd get
-# ┌ a g m s ┐
-# │ b h n t │
-# │ c i o u │
-# │ d j p v │
-# │ e k q w │
-# └ f l r x ┘.
-# These columns are treated as independent by the model, which means that the
-# dependence of e. g. 'g' on 'f' can not be learned, but allows more efficient
-# batch processing.
-
-#    import torchtext
-#    from torchtext.experimental.datasets import WikiText103 as WikiData
-#    from torchtext.experimental.datasets import WikiText2 as WikiData
-    import torchtext
-    from data import SQuAD
     try:
         vocab = torch.load(args.save_vocab)
     except:
@@ -212,34 +146,20 @@ if __name__ == "__main__":
     pad_id = vocab.stoi['<pad>']
     train_dataset, dev_dataset = SQuAD(vocab=vocab)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-#    eval_batch_size = 16
-#    train_data = batchify(train_dataset.data, args.batch_size)
-#    dev_data = batchify(dev_dataset.data, eval_batch_size)
-
-
-
-
-#    dataloader = DataLoader(train_dataset, batch_size=args.batch_size, collate_fn=pad_squad_data)
-#
-#for idx, (seq, ans_pos) in enumerate(dataloader):
-#    print(idx, seq.size(), ans_pos.size(), ans_pos)
-
-
-
-
-#    test_data = batchify(test_dataset.data, eval_batch_size)
 
     ###############################################################################
     # Build the model
     ###############################################################################
 
-#    ntokens = len(train_dataset.get_vocab())
     pretrained_bert = torch.load(args.bert_model)
-    model = model.QuestionAnswerTask(pretrained_bert).to(device)
+    model = QuestionAnswerTask(pretrained_bert).to(device)
 
     criterion = nn.CrossEntropyLoss()
 
+    ###############################################################################
     # Loop over epochs.
+    ###############################################################################
+
     lr = args.lr
     optimizer = torch.optim.SGD(model.parameters(), lr=lr)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.25)
@@ -262,14 +182,15 @@ if __name__ == "__main__":
         else:
             scheduler.step()
 
+    ###############################################################################
     # Load the best saved model.
+    ###############################################################################
     with open(args.save, 'rb') as f:
         model = torch.load(f)
-        # after load the rnn params are not a continuous chunk of memory
-        # this makes them a continuous chunk, and will speed up forward pass
-        # Currently, only rnn model supports flatten_parameters function.
 
+    ###############################################################################
     # Run on test data.
+    ###############################################################################
     test_loss = evaluate(dev_dataset)
     print('=' * 89)
     print('| End of training | test loss {:5.2f} | test ppl {:8.2f}'.format(
